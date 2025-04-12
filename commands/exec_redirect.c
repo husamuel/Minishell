1728335c3bc61c775dc3c47545e0e26d49c8cb41
husamuel <hugoleonor10@gmail.com>
2025-04-12 10:15:40 +0100

    12/04

--- commands/exec_redirect.c
+++ commands/exec_redirect.c
@@ -1,17 +1,40 @@
 #include "./../minishell.h"
 
+void build_args_from_tokens(t_token *cmd)
+{
+    t_token *current = cmd;
+    int count = 0;
+
+    while (current && current->type != CMD_REDIRECT_OUT && current->type != CMD_REDIRECT_IN
+         && current->type != CMD_HEREDOC && current->type != CMD_PIPE)
+    {
+        count++;
+        current = current->next;
+    }
+
+    cmd->args = malloc(sizeof(char *) * (count + 1));
+    if (!cmd->args)
+        return;
+
+    current = cmd;
+    int i = 0;
+    while (current && i < count)
+    {
+        cmd->args[i++] = current->cmd;
+        current = current->next;
+    }
+    cmd->args[i] = NULL;
+}
+
 int exec_redirect(t_token *token)
 {
     int fd;
     int status;
     int original_stdout;
-    t_token *next;
-    t_token *cmd_token;
+    t_token *next = token->next;
+    t_token *cmd_token = token->prev;
 
-    next = token->next;
-    if (!next || !next->cmd)
-        return (127);
-    if (token->type != CMD_REDIRECT_OUT)
+    if (!next || !next->cmd || token->type != CMD_REDIRECT_OUT)
         return (127);
 
     if (ft_strcmp(token->cmd, ">") == 0)
@@ -27,24 +50,43 @@ int exec_redirect(t_token *token)
         return (1);
     }
 
-    cmd_token = token->prev;
     while (cmd_token && cmd_token->type != CMD_EXEC && cmd_token->type != CMD_BUILDIN)
         cmd_token = cmd_token->prev;
 
+    t_token *after_file = next->next;
+    t_token *before_redir = token->prev;
+
+    if (before_redir)
+        before_redir->next = after_file;
+    if (after_file)
+        after_file->prev = before_redir;
+
     original_stdout = dup(STDOUT_FILENO);
-    
     dup2(fd, STDOUT_FILENO);
     close(fd);
 
     if (cmd_token)
+        build_args_from_tokens(cmd_token);
+
+    if (cmd_token)
         status = execute_command(cmd_token);
-    else if (next->next && next->next->type != CMD_NONE)
-        status = execute_command(next->next);
+    else if (after_file && after_file->type != CMD_NONE)
+        status = execute_command(after_file);
     else
         status = 0;
 
     dup2(original_stdout, STDOUT_FILENO);
     close(original_stdout);
+
+    if (before_redir)
+        before_redir->next = token;
+    token->prev = before_redir;
+
+    token->next = next;
+    next->prev = token;
+    next->next = after_file;
+    if (after_file)
+        after_file->prev = next;
 
     return (status);
 }
