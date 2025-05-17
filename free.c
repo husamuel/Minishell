5f5ba7790c49e18bf256a629b245b0c54ed5659c
husamuel <hugoleonor10@gmail.com>
2025-05-17 17:47:31 +0100

    more leaks

--- free.c
+++ free.c
@@ -6,56 +6,76 @@
 /*   By: gtretiak <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 12:26:21 by gtretiak          #+#    #+#             */
-/*   Updated: 2025/05/02 17:13:51 by gtretiak         ###   ########.fr       */
+/*   Updated: 2025/05/17 16:15:00 by gtretiak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void free_args_array(char **args)
+/**
+ * Libera um array de strings e define seus elementos como NULL
+ */
+void	free_args_array(char **args)
 {
-    int i;
-    
-    if (!args)
-        return;
-    
-    i = 0;
-    while (args[i])
-    {
-        free(args[i]);
-        args[i] = NULL; // Set to NULL after freeing
-        i++;
-    }
-    free(args);
+	int	i;
+
+	if (!args)
+		return ;
+	i = 0;
+	while (args[i])
+	{
+		free(args[i]);
+		args[i] = NULL; // Set to NULL after freeing
+		i++;
+	}
+	free(args);
 }
 
-void ft_free_minishell(t_mini *ms, int code)
+/**
+ * Libera recursos associados ao minishell
+ */
+void	ft_free_minishell(t_mini *ms, int code)
 {
-    (void)code; // Ignore code for now, free all tokens
-    if (!ms)
-        return;
-
-    // Free the entire token list
-    if (ms->token)
-    {
-        free_tokens(ms->token);
-        ms->token = NULL;
-    }
-
-    // Free other minishell resources if needed
-    if (ms->input)
-        free(ms->input);
-    if (ms->prompt)
-        free(ms->prompt);
-    if (ms->export)
-        free_env_list(ms->export);
-    if (ms->state)
-    {
-        free(ms->state);
-        ms->state = NULL;
-    }
+	if (!ms)
+		return ;
+	
+	// Free the entire token list
+	if (ms->token)
+	{
+		free_tokens(ms->token);
+		ms->token = NULL;
+	}
+	
+	// Free other minishell resources if needed
+	if (ms->input)
+	{
+		free(ms->input);
+		ms->input = NULL;
+	}
+	
+	if (ms->prompt)
+	{
+		free(ms->prompt);
+		ms->prompt = NULL;
+	}
+	
+	if (code != -1)
+	{
+		// Libera recursos globais apenas se for saída final
+		if (ms->export)
+		{
+			free_env_list(ms->export);
+			ms->export = NULL;
+		}
+		
+		// Não liberar state se for alocado na stack (que parece ser o caso)
+		// Se mudar isso, verifique se state é alocado com malloc em algum lugar
+	}
 }
 
+/**
+ * Libera uma lista de variáveis de ambiente
+ */
 void	free_env_list(t_env *head)
 {
 	t_env	*current;
@@ -74,24 +94,92 @@ void	free_env_list(t_env *head)
 	}
 }
 
-void free_token_content(t_token *token)
+/**
+ * VERSÃO CORRIGIDA: Libera conteúdo de um token com cuidado para evitar double free
+ */
+void	free_token_content(t_token *token)
 {
-    if (token->cmd)
-    {
-        // Only free cmd if it's not referenced by args or args_file
-        if (token->args && token->args[0] != token->cmd &&
-            token->args_file && token->args_file[0] != token->cmd)
-        {
-            free(token->cmd);
-            token->cmd = NULL;
-        }
-    }
-    if (token->args_file)
-        free_args_array(token->args_file);
-    if (token->args)
-        free_args_array(token->args);
+	if (!token)
+		return ;
+	
+	// Primeiro, verificar e marcar todos os ponteiros compartilhados
+	// para evitar double free
+	int has_shared_cmd = 0;
+	
+	// Verificar se cmd é usado em args ou args_file
+	if (token->cmd && token->args)
+	{
+		int i = 0;
+		while (token->args[i])
+		{
+			if (token->args[i] == token->cmd)
+			{
+				has_shared_cmd = 1;
+				break;
+			}
+			i++;
+		}
+	}
+	
+	if (!has_shared_cmd && token->cmd && token->args_file)
+	{
+		int i = 0;
+		while (token->args_file[i])
+		{
+			if (token->args_file[i] == token->cmd)
+			{
+				has_shared_cmd = 1;
+				break;
+			}
+			i++;
+		}
+	}
+	
+	// Liberar args_file (mas não seus conteúdos que possam ser compartilhados)
+	if (token->args_file)
+	{
+		int i = 0;
+		while (token->args_file[i])
+		{
+			// Liberar apenas se não for ponteiro compartilhado com cmd
+			if (token->cmd && token->args_file[i] == token->cmd)
+				; // Não liberar, será liberado depois
+			else
+				free(token->args_file[i]);
+			i++;
+		}
+		free(token->args_file);
+		token->args_file = NULL;
+	}
+	
+	// Liberar args (mas não seus conteúdos que possam ser compartilhados)
+	if (token->args)
+	{
+		int i = 0;
+		while (token->args[i])
+		{
+			// Liberar apenas se não for ponteiro compartilhado com cmd
+			if (token->cmd && token->args[i] == token->cmd)
+				; // Não liberar, será liberado depois
+			else
+				free(token->args[i]);
+			i++;
+		}
+		free(token->args);
+		token->args = NULL;
+	}
+	
+	// Agora é seguro liberar cmd
+	if (token->cmd)
+	{
+		free(token->cmd);
+		token->cmd = NULL;
+	}
 }
 
+/**
+ * Libera uma lista de tokens
+ */
 void	free_tokens(t_token *token)
 {
 	t_token	*tmp;
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: gtretiak <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 12:17:06 by gtretiak          #+#    #+#             */
-/*   Updated: 2025/05/02 16:56:23 by gtretiak         ###   ########.fr       */
+/*   Updated: 2025/05/17 16:00:00 by gtretiak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,35 +20,47 @@ static void	ft_init_parser(t_parser *state)
 	state->cmd_seen = 0;
 }
 
+static void	main_loop(t_mini *ms, t_parser *state)
+{
+	int	result;
+
+	while (1)
+	{
+		ms->input = get_input(ms, ms->prompt);
+		if (!ms->input)
+			break;
+		ms->token = lexer(ms->input);
+		if (ms->token)
+		{
+			result = parser(state, ms);
+			if (result == 0)
+				ft_handle_zero(ms);
+			exec(ms);
+			free_tokens(ms->token);
+			ms->token = NULL;
+		}
+		free(ms->input);
+		ms->input = NULL;
+		ft_update_ms(ms);
+	}
+}
+
 int	main(int argc, char *argv[], char **envp)
 {
 	t_mini	ms;
 	t_parser	state;
-	int		result;
 
 	(void)(argc);
 	(void)(argv);
-	result = 0;
+	
 	ms = init(envp);
 	ft_init_parser(&state);
 	ms.state = &state;
 	disable_ctrl_backslash();
 	setup_signals();
-	while (1)
-	{
-		ms.input = get_input(&ms, ms.prompt);
-		ms.token = lexer(ms.input);
-		if (ms.token)
-		{
-			result = parser(&state, &ms);
-			if (result == 0)
-				ft_handle_zero(&ms);
-			exec(&ms);
-			free_tokens(ms.token);
-		}
-		ft_update_ms(&ms);
-	}
-	free_env_list(ms.export);
+	main_loop(&ms, &state);
+	if (ms.export)
+		free_env_list(ms.export);
 	ft_free_minishell(&ms, 0);
 	exit(EXIT_SUCCESS);
 }
