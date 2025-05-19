21f24095f877a4002b3142e8f87f6d1fcd4dd3fd
Georgii Tretiakov <george22021988@gmail.com>
2025-05-19 17:47:50 +0100

    execvp (prohibit) -> execve (allowed)

--- commands/exec_pipe_execution.c
+++ commands/exec_pipe_execution.c
@@ -6,7 +6,7 @@
 /*   By: gtretiak <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 14:34:46 by gtretiak          #+#    #+#             */
-/*   Updated: 2025/04/27 14:40:54 by gtretiak         ###   ########.fr       */
+/*   Updated: 2025/05/19 17:44:17 by gtretiak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,26 +55,66 @@ static int	ft_execute_special_in_child(t_token *current, t_mini *ms)
 	return (-1);
 }
 
-static void	execute_child_process(t_token *current, t_mini *ms)
+void	execute_child_process(t_token *current, t_mini *ms)
 {
 	char	*argv[2];
 	int		is_special;
+	char	*path;
+	char	**envp;
 
 	signal(SIGINT, SIG_DFL);
 	signal(SIGQUIT, SIG_DFL);
 	is_special = ft_execute_special_in_child(current, ms);
 	if (is_special != -1)
 		exit(is_special);
+	envp = env_to_array(ms->export);
+	if (!envp)
+		exit(1);
 	if (current->args && current->args[0])
 	{
-		execvp(current->args[0], current->args);
+		path = find_command_path(current->args[0], ms);
+		if (!path)
+		{
+			ft_putstr_fd("minishell: ", 2);
+			ft_putstr_fd(current->args[0], 2);
+			ft_putstr_fd(": command not found\n", 2);
+			free_env_array(envp);
+			exit(127);
+		}
+		execve(path, current->args, envp);
 	}
-	else if (current->cmd)
+	else if	(current->cmd)
 	{
+		path = find_command_path(current->cmd, ms);
+		if (!path)
+		{
+			ft_putstr_fd("minishell: ", 2);
+			ft_putstr_fd(current->cmd, 2);
+			ft_putstr_fd(": command not found\n", 2);
+			free_env_array(envp);
+			exit(127);
+		}
 		argv[0] = current->cmd;
 		argv[1] = NULL;
-		execvp(current->cmd, argv);
+		execve(path, argv, envp);
 	}
+	else
+	{
+		free_env_array(envp);
+		exit(127);
+	}
+	if (errno == EACCES)
+	{
+		ft_putstr_fd("minishell: ", 2);
+		ft_putstr_fd(path, 2);
+		ft_putstr_fd(": permission denied\n", 2);
+		free(path);
+		free_env_array(envp);
+		exit(126);
+	}
+	perror("minishell");
+	free(path);
+	free_env_array(envp);
 	exit(127);
 }
 
