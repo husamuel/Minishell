31232786108c8a7c4485dac9977c391afa799095
Georgii Tretiakov <george22021988@gmail.com>
2025-05-19 16:34:13 +0100

    cmd status corrected

--- executable.c
+++ executable.c
@@ -6,7 +6,7 @@
 /*   By: gtretiak <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 13:28:59 by gtretiak          #+#    #+#             */
-/*   Updated: 2025/05/19 14:36:38 by gtretiak         ###   ########.fr       */
+/*   Updated: 2025/05/19 16:33:08 by gtretiak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "minishell.h"
@@ -23,11 +23,11 @@ int	ft_handle_token(t_token *current, t_token *prev, t_mini *ms)
 		&& (!prev || prev->type != CMD_EXPR))
 	{
 		printf("command not found: %d\n", ms->exit_status);
-		ms->exit_status = 127;
+//		ms->exit_status = 127;
 	}
 	else if ((!prev || prev->type != CMD_EXPR) && current->type != CMD_PLUS)
 	{
-		ms->exit_status = 127;
+//		ms->exit_status = 2;
 		return (1);
 	}
 	return (0);
