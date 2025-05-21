13bb683b0032476fb260f3ace3e6a2740dbe5499
husamuel <hugoleonor10@gmail.com>
2025-05-21 16:17:50 +0100

    expr

--- expr_processing.c
+++ expr_processing.c
@@ -6,7 +6,7 @@
 /*   By: husamuel <husamuel@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 16:02:04 by gtretiak          #+#    #+#             */
-/*   Updated: 2025/05/21 12:31:33 by gtretiak         ###   ########.fr       */
+/*   Updated: 2025/05/21 16:16:34 by husamuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,14 @@ int	process_expression(t_token *tokens, t_mini *ms)
 	{
 		symbol = current->cmd;
 		if (ft_is_valid_operator(symbol))
+		{
+			if (current->next == NULL)
+			{
+				printf("expr: missing operand after '%s'\n", symbol);
+				break ;
+			}
 			result = handle_operator(symbol, &current, result, ms);
+		}
 		else
 		{
 			ft_handle_unknown_operator(current);
@@ -39,6 +46,7 @@ int	process_expression(t_token *tokens, t_mini *ms)
 	}
 	return (result);
 }
+
 
 void	mark_tokens_as_processed(t_token *start)
 {
