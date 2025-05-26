49d4b5ef87b6ce0e47a07a38c41445fafcf14be3
Georgii Tretiakov <george22021988@gmail.com>
2025-05-26 14:34:55 +0100

    the forgotten one

--- exec/exec.c
+++ exec/exec.c
@@ -6,16 +6,32 @@
 /*   By: husamuel <husamuel@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 10:21:32 by husamuel          #+#    #+#             */
-/*   Updated: 2025/05/25 11:20:37 by husamuel         ###   ########.fr       */
+/*   Updated: 2025/05/26 14:22:10 by gtretiak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../minishell.h"
 
+static void	exec_end(t_mini *ms)
+{
+	t_token	*current;
+
+	current = ms->token;
+	while (current)
+	{
+		if (is_valid_command(current))
+		{
+			restore_child_tty();
+			ms->exit_status = execute_token(current, ms);
+		}
+		else
+			ms->exit_status = 127;
+		current = current->next;
+	}
+}
+
 void	exec(t_mini *ms)
 {
-	t_token		*current;
-
 	if (!ms || !ms->token)
 	{
 		ms->exit_status = 0;
@@ -25,19 +41,6 @@ void	exec(t_mini *ms)
 	if (ms->pipe > 0)
 		ms->exit_status = execute_pipeline(ms);
 	else
-	{
-		current = ms->token;
-		while (current)
-		{
-			if (is_valid_command(current))
-			{
-				restore_child_tty();
-				ms->exit_status = execute_token(current, ms);
-			}
-			else
-				ms->exit_status = 127;
-			current = current->next;
-		}
-	}
+		exec_end(ms);
 	reset_signals();
 }
