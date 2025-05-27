d6154ab896f2f72084182c7648ba5af53bf501dd
Georgii Tretiakov <gtretiak@c1r2s3.42porto.com>
2025-05-27 16:26:09 +0100

    numeric argument message for long long

--- builtin/exec_exit.c
+++ builtin/exec_exit.c
@@ -6,15 +6,49 @@
 /*   By: husamuel <husamuel@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 19:08:56 by gtretiak          #+#    #+#             */
-/*   Updated: 2025/05/22 12:43:19 by husamuel         ###   ########.fr       */
+/*   Updated: 2025/05/27 16:24:08 by gtretiak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../minishell.h"
 
+static unsigned long long	ft_atoll(const char *str)
+{
+	unsigned long long	n;
+	unsigned int		i;
+
+	i = 0;
+	n = 0;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == 43 || str[i] == 45)
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		n *= 10;
+		n = n + str[i] - '0';
+		i++;
+	}
+	return (n);
+}
+
+static void	ft_spec_exit(t_mini *ms, t_token *token)
+{
+	if (!ft_strncmp(token->args[1], "-9223372036854775808", 21))
+		ms->exit_status = 0;
+	else
+	{
+		printf("minishell: exit: %s: "
+			"numeric argument required\n", token->args[1]);
+		ms->exit_status = 2;
+	}
+	exit(ms->exit_status);
+}
+
 void	exec_exit(t_token *token, t_mini *ms)
 {
-	int	i;
+	unsigned long long	num;
+	unsigned int		i;
 
 	i = 0;
 	printf("exit\n");
@@ -22,15 +56,14 @@ void	exec_exit(t_token *token, t_mini *ms)
 	{
 		if (token->args[1][0] == '-')
 			i++;
+		if (!ft_strncmp("-9223372036854775808", token->args[1], 21))
+			ft_spec_exit(ms, token);
+		num = ft_atoll(token->args[1] + i);
 		while (token->args[1][i])
 		{
-			if (!ft_isdigit(token->args[1][i]))
-			{
-				printf("minishell: exit: %s: "
-					"numeric argument required\n", token->args[1]);
-				ms->exit_status = 2;
-				exit(ms->exit_status);
-			}
+			if (!ft_isdigit(token->args[1][i])
+				|| num > 9223372036854775807)
+				ft_spec_exit(ms, token);
 			i++;
 		}
 		ms->exit_status = ft_atoi(token->args[1]);
